6fd4d9a3b59b0c5fef0e514b8e7d5b6ebfffc098
YQhj <yqhj@YQhjdeiMac.local>
2015-12-21 11:40:54 -0800

    Initial Commit

---
+++ jiemianDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jiemianDemo
+//
+//  Created by  YQhj on 15/12/21.
+//  Copyright (c) 2015年  YQhj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jiemianDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jiemianDemo
+//
+//  Created by  YQhj on 15/12/21.
+//  Copyright (c) 2015年  YQhj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
